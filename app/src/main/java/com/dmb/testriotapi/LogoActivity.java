e34f0ecaa073b94fad74a616bdb99fb53df14c95
Ric <ricardoborrull97@gmail.com>
2018-02-07 23:29:23 +0100

    "Splash Art Cambiado"

--- app/src/main/java/com/dmb/testriotapi/LogoActivity.java
+++ app/src/main/java/com/dmb/testriotapi/LogoActivity.java
@@ -2,9 +2,12 @@ package com.dmb.testriotapi;
 
 import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
+import android.os.Build;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
 
 public class LogoActivity extends AppCompatActivity {
@@ -17,6 +20,11 @@ public class LogoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logo);
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window w = getWindow(); // in Activity's onCreate() for instance
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+
         handler=new Handler();
         handler.postDelayed(new Runnable() {
             @Override
@@ -25,6 +33,6 @@ public class LogoActivity extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        },3600);
+        },4000);
     }
 }

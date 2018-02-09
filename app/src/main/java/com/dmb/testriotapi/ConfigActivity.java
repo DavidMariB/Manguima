3f58a492338d5a04195a9c525719d6e2900a18eb
MaVerdejoP <maverdejop@gmail.com>
2018-02-09 17:26:37 +0100

    Cambios Strings Traduccion //Miguel Verdejo

--- app/src/main/java/com/dmb/testriotapi/ConfigActivity.java
+++ app/src/main/java/com/dmb/testriotapi/ConfigActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 
 import com.dmb.testriotapi.R;
 
-public class ConfigActivity extends AppCompatActivity {
+public class ConfigActivity extends MainActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
